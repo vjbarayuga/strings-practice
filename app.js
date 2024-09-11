20a036287cfa63b2ab470b0755d13f113fa33362
vjdbarayuga <vjdbarayuga@gmail.com>
2024-09-12 07:21:31 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+//CONCATENATION
+// let firstName = 'TechVI';
+// let lastName = 'WebDev';
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+//APPEND
+// let firstName = 'TechVI';
+// firstName += ' something else ';
+// console.log(firstName);
+
+//LENGTH
+// let firstName = 'TechVI DATA STRUCTURES';
+// console.log(firstName.length);
+
+//CHANGING CASE
+// let firstName = 'TechVI DATA STRUCTURES dsa';
+// console.log(firstName.toLowerCase());
+
+//SLICE
+// let firstName = 'TechVI DATA STRUCTURES dsa';
+// console.log(firstName.slice(0, 10));
+
+//SPLIT AND JOIN
+// let firstName = 'TechVI';
+// let lastName = 'WebDev';
+// let fullName = firstName + ' ' + lastName;
+// let splitArray = fullName.split('');
+// console.log(splitArray);
+// let joinedString = splitArray.join('-');
+// console.log(joinedString);
+
+//INCLUDES
+// let firstName = 'TechVI';
+// let lastName = 'WebDev';
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName.includes('hello'));
+
+//TRIM
+let stringWithWhitespace = '  Hello, world!   ';
+let trimmedStart = stringWithWhitespace.trimStart();
+let trimmedEnd = stringWithWhitespace.trimEnd();
+console.log(trimmedStart);
+console.log(trimmedEnd);
